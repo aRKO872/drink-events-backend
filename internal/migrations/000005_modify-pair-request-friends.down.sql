596d8b9238db1f41c3795196d4402319082b777f
aRKO872 <arkomitra872@gmail.com>
2024-08-12 18:36:58 +0530

    added modifications to pair-request and friends table

---
+++ internal/migrations/000005_modify-pair-request-friends.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE pair_requests DROP COLUMN created_at;
+ALTER TABLE pair_requests DROP COLUMN updated_at;
+
+ALTER TABLE friends DROP COLUMN created_at;
+ALTER TABLE friends DROP COLUMN updated_at;
